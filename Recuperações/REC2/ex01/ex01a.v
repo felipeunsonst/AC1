29d8c5a47637f924bf18bc5f3725f93fa1f09b98
Felipe Costa Unsonst <161458493+felipeunsonst@users.noreply.github.com>
2025-08-04 19:09:14 -0300

    Delete Recuperações directory

--- Recuperações/REC2/ex01/ex01a.v
+++
@@ -1,9 +0,0 @@
-//FELIPE COSTA UNSONST 854155
-module Da_minterms(
-    input Db, Da, x,
-    output Da_next
-);
-    assign Da_next = (~Db & Da & ~x) | (Db & ~Da & x) | (Db & Da);
-endmodule
-
-    
